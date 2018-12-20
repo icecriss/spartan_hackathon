7932112fe8957bf65a7b981e2739513b310df1e9
Chris Chatroux <icecriss@hotmail.com>
2018-12-20 09:34:42 +0100

    some style added to the Login component

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -7,41 +7,44 @@ import logoWelcomeNoir from '../pictures/welcomeLogoNoir.png'
 export default class Login extends Component {
   constructor(props) {
     super(props)
-      this.state = {
-        username:"",
-        password:""
-      }
-
+    this.state = {
+      username: "",
+      password: ""
     }
+
+  }
   render() {
     return (
       <div className="login-container">
         <img className="login-logo" src={logoWelcomeNoir} alt="perception-logo" />
-        
+
         <div className="login-field-container">
-          <input 
-            type="text" 
-            value={this.state.username} 
-            name="text" 
-            class="username-field" 
-            id="username-field" 
-            placeholder="    Nom d'utilisateur"
+
+          <input
+            type="text"
+            value={this.state.username}
+            name="text"
+            className="username-field"
+            id="username-field"
+            placeholder="Nom d'utilisateur"
             onChange={(e) => this.setState({ username: e.target.value })}></input>
 
-            <input 
-            type="password" 
-            value={this.state.password}  
-            name="password" 
-            class="password-field" 
-            id="password-field" 
+          <input
+            type="password"
+            value={this.state.password}
+            name="password"
+            className="password-field"
+            id="password-field"
             placeholder="Mot de passe"
-            onChange={(e) => this.setState({password: e.target.value}) }>
-            </input>
-          
+            onChange={(e) => this.setState({ password: e.target.value })}>
+          </input>
+
         </div>
 
+        <NavLink className="login-button" to="/">Entrer</NavLink>
+
       </div>
-    
+
     )
   }
 }
