e38da96df36aeb17520ffee4263ad2ef94b7e570
Reb Sen <archivesreb@gmail.com>
2018-12-20 16:42:53 +0100

    creation of challengeChoice component

---
+++ frontend/src/components/challengeChoice/challengeChoice.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './challengeChoice.scss'
+
+export default class challengeChoice extends Component {
+  render() {
+    return (
+      <div className="challengeChoice-container">
+        <div className=
+        
+      </div>
+    )
+  }
+}
